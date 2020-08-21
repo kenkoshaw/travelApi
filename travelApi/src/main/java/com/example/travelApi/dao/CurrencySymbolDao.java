3ba522b321beb2d80b6d5628384f076f10195eed
Benjamin Shaw <benjamin.shaw2@publicissapient.com>
2020-08-21 14:49:01 +0100

    added unicodes

--- travelApi/src/main/java/com/example/travelApi/dao/CurrencySymbolDao.java
+++ travelApi/src/main/java/com/example/travelApi/dao/CurrencySymbolDao.java
@@ -7,12 +7,12 @@ import java.util.Map;
 
 public class CurrencySymbolDao {
     private static Map<String, CurrencySymbol> symbols = new HashMap<String,CurrencySymbol>() {{
-        put("GBP",new CurrencySymbol("£"));
-        put("INR",new CurrencySymbol("₹"));
-        put("USD",new CurrencySymbol("$"));
-        put("EUR",new CurrencySymbol("€"));
-        put("ARS",new CurrencySymbol("$"));
-        put("RUB",new CurrencySymbol("₽"));
+        put("GBP",new CurrencySymbol("\u00A3","£"));
+        put("INR",new CurrencySymbol("\u20B9","₹"));
+        put("USD",new CurrencySymbol("\u0024","$"));
+        put("EUR",new CurrencySymbol("\u20AC","€"));
+        put("ARS",new CurrencySymbol("\u0024","$"));
+        put("RUB",new CurrencySymbol("\u20BD","₽"));
     }};
 
     public static CurrencySymbol getCurrencySymbol(String currCode) {
--- travelApi/src/main/java/com/example/travelApi/model/CurrencySymbol.java
+++ travelApi/src/main/java/com/example/travelApi/model/CurrencySymbol.java
@@ -1,13 +1,18 @@
 package com.example.travelApi.model;
 
 public class CurrencySymbol {
-    private final String symbol;
+    private final String symbol, icon;
 
-    public CurrencySymbol(String symbol) {
+    public CurrencySymbol(String symbol, String icon) {
         this.symbol = symbol;
+        this.icon = icon;
     }
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public String getIcon() {
+        return icon;
     }
 }
